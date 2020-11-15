5def145472ed9ab5d2435051340ab98099fdf1bb
Kobi Hari <kobi2294@yahoo.com>
2020-11-15 12:33:08 +0200

    end of hello-angular project

--- Day 02/hello-angular/src/app/app.component.ts
+++ Day 02/hello-angular/src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-angular';
+  // data
+  keyword: string = 'hello';
+  results: string[] = [];
+  isBusy: boolean = false;
+
+  // actions
+  changeKeyword(word: string) {
+    this.keyword = word;
+    console.log(word);
+  }
+
+  search() {
+    this.isBusy = true;
+    this.results = [];
+
+    setTimeout(() => {
+      this.isBusy = false;
+      this.results = [
+        this.keyword + '1', 
+        this.keyword + '2', 
+        this.keyword + '3'
+      ];
+    }, 3000);
+  }
+
 }
+
+
+
